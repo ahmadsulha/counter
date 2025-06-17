b204b9ac1e9214963955ef7253fa490069f3a822
Ahmad Sulha <sulha_ps3@yahoo.com>
2025-06-17 22:07:10 +0800

    Add vibration every 100 counts.

--- src/components/features/counter/Counter.tsx
+++ src/components/features/counter/Counter.tsx
@@ -1,8 +1,12 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import styles from './Counter.module.css'
 
 export function Counter() {
     const [count, setCount] = useState(0)
+
+    useEffect(() => {
+        if (count != 0 && count % 100 == 0 && 'vibrate' in navigator) navigator.vibrate(500)
+    }, [count])
 
     return (
         <button className={styles.button} onClick={() => setCount((count) => count + 1)}>
